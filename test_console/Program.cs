648b8b5abf129ab1850ea040e80a8971a6413496
Deven Damiano <damiano.da96@gmail.com>
2019-03-04 11:04:29 -0500

    first commit

---
+++ test_console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace test_console
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int ID { get; set; }
+
+        public User(string name, string email, int id)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.ID = id;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Random C# things
+            User[] users = new User[5];
+            string name;
+
+            for(int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine("Enter my name, boss.");
+                name = Console.ReadLine();
+                users[i] = new User(name, "esh", i);
+
+            }
+
+            for(int i = 0; i < users.Length; i++)
+            {
+                Console.WriteLine("Name: {0}" , users[i].Name);
+            }
+        }
+    }
+}
