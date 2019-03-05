706013845d322c076a6f53b6a45bbe85aa911949
Deven Damiano <damiano.da96@gmail.com>
2019-03-05 09:42:18 -0500

    added some sorting algorithms

--- test_console/Program.cs
+++ test_console/Program.cs
@@ -18,10 +18,95 @@ namespace test_console
 
     class MainClass
     {
+        public static void Swap(int[] nums, int pos1, int pos2)
+        {
+            int temp = nums[pos1];
+            nums[pos1] = nums[pos2];
+            nums[pos2] = temp;
+
+            //return nums;
+        }
+
+        public static int GetMin(int[] nums, int start)
+        {
+            int min_pos = start;
+
+            for(int i = start; i < nums.Length; i++)
+            {
+                if(nums[i] < nums[min_pos])
+                {
+                    min_pos = i;
+                }
+            }
+
+            return min_pos;
+        }
+
+        public static int[] SelectionSort(int[] nums) // O(n^2)
+        {
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                int k = GetMin(nums, i);
+
+                if(i != k)
+                {
+                    Swap(nums, i, k);
+                }
+            }
+
+            return nums;
+        }
+
+        public static void Quicksort(int[] nums, int l, int r)
+        {
+            int i, j, x;
+
+            i = l;
+            j = r;
+
+            x = nums[(l + r) / 2]; // pivot element
+
+            while(true)
+            {
+                while(nums[i] < x)
+                {
+                    i++;
+                }
+                while(x < nums[j])
+                {
+                    j--;
+                }
+                if(i <= j)
+                {
+                    Swap(nums, i, j);
+                    i++;
+                    j--;
+                }
+                if(i > j)
+                {
+                    break;
+                }
+            }
+            if(l < j)
+            {
+                Quicksort(nums, l, j);
+            }
+            if(i < r)
+            {
+                Quicksort(nums, i, r);
+            }
+        }
+
+
+        public static void Quicksort(int[] nums) // O(Nlog(N))
+        {
+            Quicksort(nums, 0, nums.Length - 1);
+        }
+
         public static void Main(string[] args)
         {
             // Random C# things
-            User[] users = new User[5];
+            /*User[] users = new User[5];
             string name;
 
             for(int i = 0; i < users.Length; i++)
@@ -35,7 +120,21 @@ namespace test_console
             for(int i = 0; i < users.Length; i++)
             {
                 Console.WriteLine("Name: {0}" , users[i].Name);
+            }*/
+
+            int[] nums = { 1, 3, 6, 2, 0, 5, 9 };
+
+            //nums = SelectionSort(nums);
+            Quicksort(nums);
+
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
             }
+
+            // sort this as fast as possible
+
+
         }
     }
 }
